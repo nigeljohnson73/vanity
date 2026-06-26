be4096ee24e68de000eaa85fce5a012b978ccbe9
Nigel <nigel@nigeljohnson.net>
2026-06-26 21:18:52 +0100

    Vanity control done

--- sh/find_tor.sh
+++ sh/find_tor.sh
@@ -2,25 +2,40 @@
 
 hostname=$(hostname)
 ipaddr=$(hostname -I | awk '{print $1;}')
+keyset="tor"
 
 root=`realpath $0`
 root=`dirname $root`
 root=`realpath $root/..`
 echo "Working directory: $root"
 
-if [ -z "$1" ]; then
-	echo "$0 <token>"
-	exit
-fi
-
-run=`ps -ef | grep tor_vanity | grep -v grep`
+run=`ps -ef | grep "vanity_$keyset" | grep -v grep`
 
 if [ ! -z "$run" ]; then
 	echo "toolset already running"
 else
-	mkdir -p $root/keys_tor
-	cd $root/keys_tor
-	$root/sh/tor_vanity $1 -v -n 1 -d . -t 2 -s
-	$root/sh/phonehome "$hostname($ipaddr): tor vanity lookup completed for '$1'"
+	mkdir -p $root/keys_$keyset
+	while [ 1 ]; do
+		threads=$($root/sh/vanityctl thget $keyset)
+		key=$($root/sh/vanityctl current $keyset)
+		if [ -z "$key" ]; then
+			echo "$keyset search complete"
+			exit
+		else
+			cd $root/keys_$keyset
+			if [ "$keyset" = "i2p" ]; then
+				$root/sh/vanity_i2p $key -t $threads
+				#echo "$root/sh/vanity_i2p $key -t $threads"
+			fi
+			if [ "$keyset" = "tor" ]; then
+				$root/sh/vanity_tor $key -v -n 1 -d . -t $threads -s
+				#echo "$root/sh/vanity_tor $key -v -n 1 -d . -t $threads -s"
+			fi
+			#sleep 5
+			$root/sh/phonehome "$hostname($ipaddr): $keyset vanity lookup completed for '$key'"
+			#echo "$hostname($ipaddr): $keyset vanity lookup completed for '$key'"
+			$root/sh/vanityctl remove $keyset $key
+		fi
+	done
 fi
 
--- sh/vanityctl.py
+++ sh/vanityctl.py
@@ -8,7 +8,7 @@ def usage():
     print()
     print(f"{sys.argv[0]} <cmd> <base> [<val>]")
     print()
-    print("    cmd  - add, remove, empty, current, thset, thget")
+    print("    cmd  - add, remove, empty, current, thset, thget, start, stop")
     print("    base - i2p, tor, both")
     print("    val  - data value if add, remove, thset")
     print()
@@ -41,11 +41,17 @@ def writeFile(base, params):
         pass
     return False
 
+def stop(base):
+    os.system("killall find_i2p.sh find_tor.sh vanity_i2p vanity_tor > /dev/null 2>&1")
+
+def start(base):
+    os.system(f"nohup sh/find_{base}.sh > /tmp/output_{b}.txt &")
+
 if len(sys.argv) < 3:
     error("Invalid number of arguments")
 
 cmd=sys.argv[1].lower()
-if cmd not in [ 'add', 'remove', 'empty', 'current', 'thset', 'thget' ]:
+if cmd not in [ 'add', 'remove', 'empty', 'current', 'thset', 'thget', 'start', 'stop' ]:
     error(f"Unknown command '{cmd}'")
 
 base=[ sys.argv[2].lower() ]
@@ -79,54 +85,61 @@ if cmd in [ "thset" ]:
         error(f"Invalid number of threads '{val}'")
 
 for b in base:
-    params = readFile(b)
-    #print(params)
-
-    if cmd in [ "current", "thget" ]:
-        if cmd == "thget":
-            val = 2
-            try:
-                val = params.get("threads")
-            except:
-                pass
-
-        if cmd == "current":
-            val = ""
-            try:
-                val = params.get("vals")[0]
-            except:
-                pass
-
-        print(val)
+    if cmd in [ "start", "stop" ]:
+        if cmd == "start":
+            start(b)
+        if cmd == "stop":
+            stop(b)
 
     else:
-        if cmd == "thset":
-            params.update({"threads":val})
+        params = readFile(b)
+        #print(params)
 
-        if cmd == "empty":
-            params.update({"vals":[]})
+        if cmd in [ "current", "thget" ]:
+            if cmd == "thget":
+                val = 2
+                try:
+                    val = params.get("threads")
+                except:
+                    pass
 
-        if cmd == "add":
-            vals = params.get("vals")
-            if val in vals:
-                print(f"Value {val} already on list for {base} - ignoring update")
-            else:
-                vals.append(val)
-                params.update({"vals":vals})
+            if cmd == "current":
+                val = ""
+                try:
+                    val = params.get("vals")[0]
+                except:
+                    pass
 
-        if cmd == "remove":
-            vals = params.get("vals")
-            if val not in vals:
-                print(f"Value {val} not in list for {base} - ignoring update")
-            else:
-                vals.remove(val)
-                params.update({"vals":vals})
+            print(val)
 
-        #json_string = json.dumps(params)
-        #print(json_string)
-
-        if writeFile(b, params):
-            print("Data updated")
         else:
-            print("Failed to update data")
+            if cmd == "thset":
+                params.update({"threads":val})
+
+            if cmd == "empty":
+                params.update({"vals":[]})
+
+            if cmd == "add":
+                vals = params.get("vals")
+                if val in vals:
+                    print(f"Value {val} already on list for {base} - ignoring update")
+                else:
+                    vals.append(val)
+                    params.update({"vals":vals})
+
+            if cmd == "remove":
+                vals = params.get("vals")
+                if val not in vals:
+                    print(f"Value {val} not in list for {base} - ignoring update")
+                else:
+                    vals.remove(val)
+                    params.update({"vals":vals})
+
+            #json_string = json.dumps(params)
+            #print(json_string)
+
+            if writeFile(b, params):
+                print("Data updated")
+            else:
+                print("Failed to update data")
 
