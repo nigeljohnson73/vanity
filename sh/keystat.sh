8653cd58adaa9a8b6967c4174556daabb8ac0625
Nigel <nigel@nigeljohnson.net>
2026-06-27 06:46:46 +0100

    Added temp and throttle status

--- sh/keystat.sh
+++ sh/keystat.sh
@@ -9,6 +9,8 @@ hostname=$(hostname)
 ipaddr=$(hostname -I | awk '{print $1;}')
 i2prun=$(ps -ef | grep vanity_i2p | grep -v grep)
 torrun=$(ps -ef | grep vanity_tor | grep -v grep)
+temp=$(vcgencmd measure_temp)
+throttled=$(vcgencmd get_throttled)
 
 if [ -z "$torrun" ]; then
 	torrun="DN"
@@ -21,8 +23,8 @@ else
 	i2prun="UP"
 fi
 
-msg="$hostname($ipaddr): TOR-$torrun; I2P-$i2prun;"
+msg="$hostname($ipaddr): TOR=$torrun; I2P=$i2prun; $temp; $throttled"
 echo $msg
-$root/sh/phonehome "$msg"
+#$root/sh/phonehome "$msg"
 
 
